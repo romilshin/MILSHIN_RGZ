b6a499a374a9887390fc5a793955840b144523b7
рома <roma@MacBook-Air-roma.local>
2026-04-28 14:30:00 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: '179р',
+        bestseller: true,
+        stock: true
+    },
+     {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price:'499р',
+        bestseller: false,
+        stock: true
+    },
+     {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price:'549р',
+        bestseller: false,
+        stock: false
+    },
+    {
+        author: 'Виктор Пелевин',
+        name: 'Возвращение Синей Бороды',
+        publicationYear: 2026,
+        price:'499р',
+        bestseller: true,
+        stock: false
+    },
+    {
+        author: 'Энди Вейер',
+        name: 'Проект "Аве Мария"',
+        publicationYear: 2021,
+        price:'529р',
+        bestseller: false,
+        stock: true
+    },
+    {
+        author: 'Пенн Коул',
+        name: 'Сияние вечного пламени',
+        publicationYear: 2022,
+        price:'449р',
+        bestseller: false,
+        stock: true
+    },
+    {
+        author: 'Элис Нокс',
+        name: 'Двор Истлевших Сердец',
+        publicationYear: 2026,
+        price:'299р',
+        bestseller: false,
+        stock: false
+    },
+    {
+        author: 'Стив Кавана',
+        name: 'Защита',
+        publicationYear: 2015,
+        price:'399р',
+        bestseller: false,
+        stock: true
+    },
+    {
+        author: 'Калли Харт',
+        name: 'Ртуть',
+        publicationYear: 2024,
+        price:'499р',
+        bestseller: false,
+        stock: false
+    },
+    {
+        author: 'Анджей Сапковский',
+        name: 'Перекресток воронов',
+        publicationYear: 2024,
+        price:'599р',
+        bestseller: true,
+        stock: true
+    },
+];
