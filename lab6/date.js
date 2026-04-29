b8b934dc3506b0f44b9f69aec7008807225a20bc
рома <roma@MacBook-Air-roma.local>
2026-04-29 12:45:04 +0700

    Вывод даты

---
+++ lab6/date.js
@@ -0,0 +1,31 @@
+const dates = [
+    {
+        country: 'России',
+        local: 'ru-RU'
+    },
+
+    {
+        country: 'Греции',
+        local: 'el-GR'
+    },
+
+    {
+        country: 'Польши',
+        local: 'pl-PL'
+    },
+
+    {
+        country: 'Турции',
+        local: 'tr-TR'
+    },
+
+    {
+        country: 'Вьетнама',
+        local: 'vi-VN'
+    },
+
+    {
+        country: 'Канады',
+        local: 'en-CA'
+    },
+]
